b7c731cf7bdc125eec4a9546b81e2c211aec805c
Taruj Goyal <taruj.goyal@abridge.com>
2025-05-16 13:33:10 -0700

    Updated sha and workflow

--- templates/clinician-k8s/main.tf
+++ templates/clinician-k8s/main.tf
@@ -42,7 +42,7 @@ locals {
 
   # Image and environment configuration
   base_image_repo = "us-central1-docker.pkg.dev/abridge-artifact-registry/coder/base"
-  base_image_tag  = "4bd7714"
+  base_image_tag  = "ea2464b"
   base_image      = "${local.base_image_repo}:${local.base_image_tag}"
   home_dir        = "/home/vscode"
 
