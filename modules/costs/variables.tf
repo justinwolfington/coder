fd5aaa10af440519abfd08b962adee6edcfd34d3
shubh3ai <shubh.gupta@abridge.com>
2025-06-17 16:54:32 -0700

    fix: convert cost from float to int

--- modules/costs/variables.tf
+++ modules/costs/variables.tf
@@ -1,14 +1,9 @@
 variable "ram_cost_per_gb" {
   type = number
-  default = 0.25
+  default = 1
 }
 
 variable "cpu_cost_per_core" {
     type = number
-    default = 0.25
-}
-
-variable "disk_cost_per_gb" {
-    type = number
-    default = 0.005
+    default = 2
 }
