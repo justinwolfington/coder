19a5723348045f509f02ac760c9908c2a8d35a6a
shubh3ai <shubh.gupta@abridge.com>
2025-06-17 17:49:43 -0700

    update: shared resource module

--- modules/resources/common_parameters.tf
+++ modules/resources/common_parameters.tf
@@ -5,18 +5,8 @@
 # across multiple templates in the coder workspace setup.
 
 # Repository URL parameter - used in all k8s templates
-data "coder_parameter" "repository_url" {
-  name         = "repository_url"
-  display_name = "Repository URL"
-  description  = "GitHub repository URL (leave empty for no repository)"
-  default      = "https://github.com/abridgeai/completion-service"
-  mutable      = true
-  order        = 1
-  type         = "string"
-}
-
 # CPU cores parameter - used in all k8s templates
-data "coder_parameter" "cpu" {
+data "coder_parameter" "cpux" {
   name         = "cpu"
   display_name = "CPU Cores"
   description  = "The number of CPU cores (between 4-16)"
@@ -30,19 +20,3 @@ data "coder_parameter" "cpu" {
     max = 16
   }
 }
-
-# Home disk size parameter - used in all k8s templates
-data "coder_parameter" "home_disk_size" {
-  name         = "home_disk_size"
-  display_name = "Home disk size (GB)"
-  description  = "The size of the home disk in GB (between 16-1024)"
-  default      = "16"
-  type         = "number"
-  icon         = "/icon/folder.svg"
-  mutable      = true
-  order        = 4
-  validation {
-    min = 16
-    max = 1024
-  }
-} 
--- modules/resources/outputs.tf
+++ modules/resources/outputs.tf
@@ -2,40 +2,7 @@
 # OUTPUTS FOR CODER PARAMETERS
 ############################
 
-# Common parameters outputs
-output "repository_url" {
-  description = "Repository URL parameter"
-  value       = data.coder_parameter.repository_url
-}
-
 output "cpu" {
   description = "CPU cores parameter"
-  value       = data.coder_parameter.cpu
+  value       = data.coder_parameter.cpux.value
 }
-
-output "home_disk_size" {
-  description = "Home disk size parameter"
-  value       = data.coder_parameter.home_disk_size
-}
-
-# Memory parameters outputs
-output "memory_standard" {
-  description = "Standard memory parameter (8-32 GB)"
-  value       = data.coder_parameter.memory_standard
-}
-
-output "memory_high" {
-  description = "High memory parameter (16-1024 GB)"
-  value       = data.coder_parameter.memory_high
-}
-
-# GPU parameters outputs
-output "gpu_accelerator" {
-  description = "GPU accelerator type parameter"
-  value       = data.coder_parameter.gpu_accelerator
-}
-
-output "gpu_count" {
-  description = "GPU count parameter"
-  value       = data.coder_parameter.gpu_count
-} 
