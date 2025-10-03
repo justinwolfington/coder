3183b189d7dbd22d31e0b7b278ea18d02dab150f
shubh3ai <shubh.gupta@abridge.com>
2025-10-03 09:53:51 +0530

    update: gcp vm images (#92)

--- templates/gcp-vm-modular/main.tf
+++ templates/gcp-vm-modular/main.tf
@@ -78,20 +78,20 @@ data "coder_parameter" "dl_image" {
   display_name = "Deep Learning Image"
   description  = "Select deep learning platform image"
   type         = "string"
-  default      = "pytorch-2-7-cu128-ubuntu-2204-nvidia-570-v20250728"
+  default      = "pytorch-2-7-cu128-ubuntu-2204-nvidia-570-v20250922"
   mutable      = true
 
   option {
     name  = "PyTorch 2.7 + CUDA 12.8 (Ubuntu 22.04)"
-    value = "pytorch-2-7-cu128-ubuntu-2204-nvidia-570-v20250728"
+    value = "pytorch-2-7-cu128-ubuntu-2204-nvidia-570-v20250922"
   }
   option {
     name  = "Multi-Framework + CUDA 12.8 (Ubuntu 22.04)"
-    value = "common-cu128-ubuntu-2204-nvidia-570-v20250728"
+    value = "common-cu128-ubuntu-2204-nvidia-570-v20250922"
   }
   option {
     name  = "Ubuntu 24.04 Noble (Clean Base)"
-    value = "ubuntu-2404-noble-amd64-v20250725"
+    value = "ubuntu-2404-noble-amd64-v20250920"
   }
 }
 
@@ -211,9 +211,9 @@ locals {
   # gcloud compute images list --project ubuntu-os-cloud --format="value(NAME)" --filter="name~ubuntu-2404"
 
   dl_images = {
-    "pytorch-2-7-cu128-ubuntu-2204-nvidia-570-v20250728" = "projects/deeplearning-platform-release/global/images/pytorch-2-7-cu128-ubuntu-2204-nvidia-570-v20250728"
-    "common-cu128-ubuntu-2204-nvidia-570-v20250728"      = "projects/deeplearning-platform-release/global/images/common-cu128-ubuntu-2204-nvidia-570-v20250728"
-    "ubuntu-2404-noble-amd64-v20250725"                  = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20250725"
+    "pytorch-2-7-cu128-ubuntu-2204-nvidia-570-v20250922" = "projects/deeplearning-platform-release/global/images/pytorch-2-7-cu128-ubuntu-2204-nvidia-570-v20250922"
+    "common-cu128-ubuntu-2204-nvidia-570-v20250922"      = "projects/deeplearning-platform-release/global/images/common-cu128-ubuntu-2204-nvidia-570-v20250922"
+    "ubuntu-2404-noble-amd64-v20250920"                  = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20250920"
   }
   image = local.dl_images[data.coder_parameter.dl_image.value]
 
