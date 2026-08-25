f4dce0bfd1eb6ec113dcfec7736f7951cffad2d5
Michael Suchacz <203725896+ibetitsmike@users.noreply.github.com>
2026-08-25 12:56:06 +0000

    feat(site): surface compaction trigger precedence

--- site/src/pages/AgentsPage/AgentChatPageView.tsx
+++ site/src/pages/AgentsPage/AgentChatPageView.tsx
@@ -30,6 +30,7 @@ import { cn } from "#/utils/cn";
 import { pageTitle } from "#/utils/page";
 import { generateUUID } from "#/utils/random";
 import { findWorkspaceAgent } from "#/utils/workspace";
+import type { ResolvedCompactionThreshold } from "./compactionTriggers";
 import {
 	AgentChatInput,
 	type ChatMessageInputRef,
@@ -154,7 +155,7 @@ interface AgentChatPageViewProps {
 	isModelCatalogLoading?: boolean;
 	planModeEnabled?: boolean;
 	onPlanModeToggle?: (enabled: boolean) => void;
-	compressionThreshold: number | undefined;
+	compactionThreshold: ResolvedCompactionThreshold | undefined;
 	isInputDisabled: boolean;
 	isSubmissionPending: boolean;
 	isInterruptPending: boolean;
@@ -352,7 +353,7 @@ export const AgentChatPageView: FC<AgentChatPageViewProps> = ({
 	isModelCatalogLoading = false,
 	planModeEnabled,
 	onPlanModeToggle,
-	compressionThreshold,
+	compactionThreshold,
 	isInputDisabled,
 	isSubmissionPending,
 	isInterruptPending,
@@ -995,7 +996,7 @@ export const AgentChatPageView: FC<AgentChatPageViewProps> = ({
 								organizationId={organizationId}
 								sendShortcut={sendShortcut}
 								store={store}
-								compressionThreshold={compressionThreshold}
+								compactionThreshold={compactionThreshold}
 								onSend={editing.handleSendFromInput}
 								onDeleteQueuedMessage={handleDeleteQueuedMessage}
 								onPromoteQueuedMessage={handlePromoteQueuedMessage}
